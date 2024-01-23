a3d82845019da299059a7f362945133785958d66
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-01-23 10:11:07 -0300

    Creation of controller method in HelloWorldController to read form data and add it to the model through attribute.

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +16,20 @@ public class HelloWorldController {
 	
 	@GetMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	@GetMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		String theName = request.getParameter("studentName");
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Yo! " + theName;
+		
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 }
